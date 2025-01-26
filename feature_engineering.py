fe76c8a5236a75fa16a0254ae1a638ce2ffc8956
ng-23 <23grattan@gmail.com>
2025-01-25 23:23:41 -0500

    major changes

--- feature_engineering.py
+++ feature_engineering.py
@@ -3,7 +3,7 @@ Dataset Features
 
 Year - year of olympics
 Host_Country - country hosting olympics
-Country - name of country competing
+Competing_Country - name of country competing per year/game
 Country_Total_Athletes - total number of athletes representing a country at olympics
 Country_Total_Medals - total gold, silver, bronze medals earned by a country
 Country_Golds - total gold medals
@@ -32,3 +32,62 @@ Country_Mixed_Events_Enrollment - number of athletes representing a country in m
 '''
 
 import pandas as pd
+
+def country_total_athletes(comap_df):
+    all_ioc = pd.read_csv('/home/noahg/competitive_ml/comap/2025/COMAP/preprocessed_data/all_ioc.csv').set_index('Nation')['Code'] # load 3-letter country codes
+    print(f'Country codes:\n{all_ioc.head(n=15)}')
+    athletes_data = pd.read_csv('/home/noahg/competitive_ml/comap/2025/COMAP/preprocessed_data/summerOly_athletes.csv')
+
+    total_athletes = []
+    for year in game_participating_countries:
+        print(f'Processing athletes for every country in {year} olympics...')
+        for country in game_participating_countries[year]:
+            curr_year_data = athletes_data.loc[(athletes_data['Year'] == year) & (athletes_data['NOC'] == all_ioc[country])]
+            athlete_names = curr_year_data['Name'].unique()
+            total_athletes.append(len(athlete_names))
+
+    comap_df['country_total_athletes'] = total_athletes
+    print(f'Updated dataframe:\n{comap_df.head(n=25)}')
+    return comap_df
+
+hosts = pd.read_csv("/home/noahg/competitive_ml/comap/2025/COMAP/preprocessed_data/summerOly_hosts_cleaned.csv")
+medal_count = pd.read_csv("/home/noahg/competitive_ml/comap/2025/COMAP/preprocessed_data/summerOly_medal_counts.csv")
+
+host_years = hosts["Year"]
+host_locations = hosts["Host"]
+
+game_host_countries = {} # maps an olympic year to its host country
+game_participating_countries = {} # maps an olympic year to a set of countries competing in it
+for year in host_years:
+    curr_year_data = medal_count.loc[(medal_count['Year'] == year)] # slice dataframe so only rows where Year = current year are included
+    game_participating_countries[year] = sorted(set(curr_year_data['NOC'])) # sort the competing countries alphabetically
+
+    curr_year_data = hosts.loc[(hosts['Year'] == year)] # slice dataframe so only rows where Year = current year are 
+    curr_year_location = curr_year_data['Host'].values[0] # get the Host column, convert it to a list of its values, then get the 1st value (a string)
+    location_parts = curr_year_location.split(',') # separate the city and the country (we only want the country)
+    game_host_countries[year] = location_parts[1] # map the olympic year to the host country
+
+print(f'1896 host country: {game_host_countries[1896]}')
+print(f'1896 competing countries: {game_participating_countries[1896]}')
+    
+init_df_dict = {'year':[], 'host_country':[], 'competing_country':[]} # dict to initialize the new dataframe from
+for year in game_participating_countries:
+    competing_countries = game_participating_countries[year]
+    num_competing_counties = len(competing_countries) # get the number of countries competing in current year's olympics
+    
+    # add countries to dataframe dict (each gets its own row)
+    for country in competing_countries:
+        init_df_dict['competing_country'].append(country)
+
+    # add year and host country to dataframe dict
+    for i in range(num_competing_counties):
+        init_df_dict['year'].append(year)
+        init_df_dict['host_country'].append(game_host_countries[year])
+comap_df = pd.DataFrame.from_dict(init_df_dict)
+print(f'Initial dataframe:\n{comap_df.head(n=25)}')
+
+# create country_total_athletes column
+comap_df = country_total_athletes(comap_df)
+comap_df.to_csv('foo.csv')
+
+
--- get_ioc.py
+++ get_ioc.py
@@ -1,25 +1,69 @@
 import pandas as pd
 
+def remove_code(names, exclude={}):
+    new_names = []
+    for name in names:
+        if name not in exclude:
+            parts = name.split(' ')
+            try:
+                from_idx = parts.index('code')
+                parts = parts[:from_idx]
+            except Exception as e:
+                pass
+            new_names.append((' '.join(parts)))
+        else:
+            new_names.append(name)
+    return new_names
+
+def remove_from(names, exclude={}):
+    new_names = []
+    for name in names:
+        if name not in exclude:
+            parts = name.split(' ')
+            try:
+                from_idx = parts.index('from')
+                parts = parts[:from_idx]
+            except Exception as e:
+                pass
+            new_names.append((' '.join(parts)))
+        else:
+            new_names.append(name)
+    return new_names
+
+def remove_From(names, exclude={}):
+    new_names = []
+    for name in names:
+        if name not in exclude:
+            parts = name.split(' ')
+            try:
+                from_idx = parts.index('From')
+                parts = parts[:from_idx]
+            except Exception as e:
+                pass
+            new_names.append((' '.join(parts)))
+        else:
+            new_names.append(name)
+    return new_names
+
 # see https://stackoverflow.com/a/70335285/
-curr_ioc = pd.read_html('https://en.wikipedia.org/wiki/List_of_IOC_country_codes')[0].set_index('Code')['National Olympic Committee']
-hist_ioc = pd.read_html('https://en.wikipedia.org/wiki/List_of_IOC_country_codes')[2].set_index('Code')['Nation/Team']
-obs_ioc = pd.read_html('https://en.wikipedia.org/wiki/List_of_IOC_country_codes')[3].set_index('Code')['Nation (NOC)']
+curr_ioc = pd.read_html('https://en.wikipedia.org/wiki/List_of_IOC_country_codes')[0]
+curr_ioc['National Olympic Committee'] = remove_From(remove_from(remove_code(curr_ioc['National Olympic Committee'])))
+curr_ioc = curr_ioc.set_index('Code')['National Olympic Committee']
+
+hist_ioc = pd.read_html('https://en.wikipedia.org/wiki/List_of_IOC_country_codes')[2]
+hist_ioc['Nation/Team'] = remove_From(remove_from(remove_code(hist_ioc['Nation/Team'])))
+hist_ioc = hist_ioc.set_index('Code')['Nation/Team']
+
+obs_ioc = pd.read_html('https://en.wikipedia.org/wiki/List_of_IOC_country_codes')[3]
+obs_ioc['Nation (NOC)'] = remove_From(remove_from(remove_code(obs_ioc['Nation (NOC)'])))
+obs_ioc = obs_ioc.set_index('Code')['Nation (NOC)']
+
 spec_ioc = pd.read_html('https://en.wikipedia.org/wiki/List_of_IOC_country_codes')[4]
-spec_nations = list(spec_ioc['Nation/team'].values)
-correct_spec_nations = []
-for nation in spec_nations:
-    if nation != 'Athletes from Kuwait':
-        # remove the from after the actual country name...
-        parts = nation.split(' ')
-        try:
-            from_idx = parts.index('from')
-            parts = parts[:from_idx]
-        except Exception as e:
-            pass
-        correct_spec_nations.append(' '.join(parts))
-    else:
-        correct_spec_nations.append(nation)
-spec_ioc['Nation/team'] = correct_spec_nations
+spec_ioc['Nation/team'] = remove_From(remove_from(remove_code(spec_ioc['Nation/team'].values), exclude=('Athletes from Kuwait')))
 spec_ioc = spec_ioc.set_index('Code')['Nation/team']
-ioc = pd.concat([curr_ioc, hist_ioc, obs_ioc, spec_ioc])
+
+special_cases = pd.DataFrame.from_dict({'Code':['TPE','TPE','CZE','CZE','IPP','MKD','CVD'], 'Nation':['Formosa','Taiwan','Czechia','Czech Republic','FR Yugoslavia','Macedonia','Cabo Verde']}).set_index('Code')['Nation']
+ioc = pd.concat([curr_ioc, hist_ioc, obs_ioc, spec_ioc, special_cases])
+ioc = ioc.drop_duplicates(keep='first')
+ioc = ioc.rename('Nation')
 ioc.to_csv('all_ioc.csv')
